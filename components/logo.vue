0a48a85236f3bd646e2da8f5cf0699db41cae55b
faraaz baig <fazzy0908@yandex.com>
2021-02-25 05:57:32 +0530

    favicon updated

--- components/logo.vue
+++ components/logo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center px-20 pt-20 pb-7">
         <nuxt-link to="/">
-            <img class="w-96" src="~/assets/images/logo-black.svg" alt="logo"/>
+            <img class="w-72" src="~/assets/images/logo-black.svg" alt="logo"/>
         </nuxt-link>
     </div>
 </template>
